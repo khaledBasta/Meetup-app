3cf7c9e3a8716ca3cf238c43c7f3ddc863c91906
Khaled Yahia <khaled.basta@protonmail.com>
2023-01-04 10:38:21 +0200

    Add getStaticProps function

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import MeetupList from "../components/meetups/MeetupList";
 
-const DUMMY_LIST = [
+const DUMMY_MEETUPS = [
   {
     id: "m1",
     title: "Programming Meetup",
@@ -19,8 +19,29 @@ const DUMMY_LIST = [
   },
 ];
 
-const HomePage = () => {
-  return <MeetupList meetups={DUMMY_LIST} />;
+const HomePage = (props) => {
+  return <MeetupList meetups={props.meetups} />;
 };
+
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+//   // Fetch data from an API
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// }
+
+export async function getStaticProps() {
+  // fetch data from an API
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+    revalidate: 10,
+  };
+}
 
 export default HomePage;
