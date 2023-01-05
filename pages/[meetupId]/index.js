38628d243ffb2387d08c1d8f14649daf18d7bdc5
Khaled Yahia <khaled.basta@protonmail.com>
2023-01-05 19:38:26 +0200

    Set Paths & SSG data fetching

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -12,4 +12,43 @@ const MeetupDetails = () => {
   );
 };
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  // Fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+        id: meetupId,
+        title: "First Meetup",
+        address: "Some Address",
+        description: "The meetup description",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
