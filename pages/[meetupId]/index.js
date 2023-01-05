9c7779722b9fca7e0d23b0c2ed9f96c1e64467bf
Khaled Yahia <khaled.basta@protonmail.com>
2023-01-05 20:48:45 +0200

    Generate array of paths dynamically from database

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,3 +1,4 @@
+import { MongoClient } from "mongodb";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 const MeetupDetails = () => {
@@ -13,25 +14,33 @@ const MeetupDetails = () => {
 };
 
 export async function getStaticPaths() {
+  // Fetch data for a single meetup
+  const client = await MongoClient.connect(
+    "mongodb://admin:12345@ac-ymz2wuj-shard-00-00.asbztbi.mongodb.net:27017,ac-ymz2wuj-shard-00-01.asbztbi.mongodb.net:27017,ac-ymz2wuj-shard-00-02.asbztbi.mongodb.net:27017/meetups?ssl=true&replicaSet=atlas-9tlr9e-shard-0&authSource=admin&retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
   return {
     fallback: false,
-    paths: [
-      {
-        params: {
-          meetupId: "m1",
-        },
+    paths: meetups.map((meetup) => ({
+      params: {
+        meetupId: meetup._id.toString(),
       },
-      {
-        params: {
-          meetupId: "m2",
-        },
-      },
-    ],
+    })),
   };
 }
 
 export async function getStaticProps(context) {
   // Fetch data for a single meetup
+  const client = await MongoClient.connect(
+    "mongodb://admin:12345@ac-ymz2wuj-shard-00-00.asbztbi.mongodb.net:27017,ac-ymz2wuj-shard-00-01.asbztbi.mongodb.net:27017,ac-ymz2wuj-shard-00-02.asbztbi.mongodb.net:27017/meetups?ssl=true&replicaSet=atlas-9tlr9e-shard-0&authSource=admin&retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
 
   const meetupId = context.params.meetupId;
 
